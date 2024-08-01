a7ee3d4bc058cd71adf552b06c03fe701a30b272
Edgar Lopez <edgarlopez755@gmail.com>
2024-07-31 20:44:27 -0700

    Exercise 5 done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,24 @@ const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.getElementById('message')
 
 
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+
+
+
+
+
+
+
 console.log(squareEls)
 
 console.log(messageEl)
